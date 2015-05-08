d75d73074884b845cc1120d12dfb5887e404be6c
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-05-08 15:06:20 -0400

    Language API changed.

--- src/js/services/measurements/languages-service.js
+++ src/js/services/measurements/languages-service.js
@@ -2,10 +2,9 @@
 	'use strict';
 	module.factory( 'Languages', function ( $log, $http, Settings ) {
 		return {
-			languages: function ( use_local_names ) {
+			languages: function () {
 				return $http.get( Settings.api.measurements( '/languages' ), {
-					params: {'use_local_names': use_local_names === true},
-					cache:  true
+					cache: true
 				} ).then( function ( response ) {
 					return response.data;
 				} );
