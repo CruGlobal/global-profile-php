73afed4ac932089dc7faad3328392e87df89336f
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-06-26 15:35:39 -0400

    Update dist files

--- dist/index.php
+++ dist/index.php
@@ -41,8 +41,8 @@
 	</div>
 
 	<!-- Application JavaScript -->
-	<script src="js/app.min.js?rev=8bcb9e52"></script>
-	<script src="js/templates.min.js?rev=781aca1d"></script>
+	<script src="js/app.min.js?rev=8982ca20"></script>
+	<script src="js/templates.min.js?rev=77f3f529"></script>
 
 	</body>
 	</html>
