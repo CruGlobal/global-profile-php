832be0853d6d82dbcb6c84ae61a6ee582a3fc7e1
Brian Zoetewey <brian.zoetewey@ccci.org>
2015-08-26 13:53:53 -0400

    [#GLOPRO-15] Show inactive ministries

--- src/js/api/measurements/ministries.service.js
+++ src/js/api/measurements/ministries.service.js
@@ -4,7 +4,7 @@
 	module.factory( 'Ministries', function ( $log, $resource, Settings ) {
 		return $resource( Settings.api.measurements( '/ministries/:ministry_id' ), {}, {
 			'systems': {method: 'GET', isArray: true, params: {global_profile_only: 'true'}, cache: true},
-			'whq':     {method: 'GET', isArray: true, params: {whq_only: 'true'}, cache: true},
+			'whq':     {method: 'GET', isArray: true, params: {whq_only: 'true', show_inactive: 'true'}, cache: true},
 			'query':   {method: 'GET', isArray: true}
 		} );
 	} );
